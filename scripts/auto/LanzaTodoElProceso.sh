e0fec47bf13c0557411413f17c074266fd8e77f5
David <dcg1002@alu.ubu.es>
2021-01-05 22:08:24 +0100

    RePush encoding error

--- scripts/auto/LanzaTodoElProceso.sh
+++ scripts/auto/LanzaTodoElProceso.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
-path=$(pwd)
-echo $path
-echo "${path/*}"
 
 # Este script se ha desarrollado para lanzar todo el proceso desde CRON.
-python3 ${path%/*}/auto/1_recabaInfo.py
-python3 ${path%/*}/auto/3_cocinado.py
-sh ${path%/*}/auto/4_reescribeCron.sh
+python3 ${PWD%/*}/auto/1_recabaInfo.py
+python3 ${PWD%/*}/auto/3_cocinado.py
+sh ${PWD%/*}/auto/4_reescribeCron.sh
